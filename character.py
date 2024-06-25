4774ea5bf0aafb0d77ce7b47ba11957bf3cb012e
zuvile <zivile.mitke@gmail.com>
2024-06-25 14:55:09 +0300

    add basic character attack

--- character.py
+++ character.py
@@ -1,18 +1,33 @@
 from pyray import *
+import random
 
 from object import Object
 
 class Character(Object):
     attack = 10
-    defence = 10
+    ac = 5
+    hp = 20
+    dead = False
+
     def __init__(self, texture, sub_texture, scale, x=0, y=0, size=32):
         super().__init__(x, y, size)
         self.texture = texture
         self.sub_texture = sub_texture
         self.scale = scale
 
+    def apply_damage(self, damage):
+        self.hp -= damage
+        if self.hp <= 0:
+            self.hp = 0
+
+    def is_alive(self):
+        return self.hp > 0
+
     def draw(self):
         destination = Rectangle(self.rec.x, self.rec.y, self.sub_texture.width * self.scale, self.sub_texture.height * self.scale)
         origin = Vector2(0, 0)
         rotation = 0.0
         draw_texture_pro(self.texture, self.sub_texture, destination, origin, rotation, WHITE)
+
+    def do_attack(self):
+        return random.randint(0, self.attack)
--- fight_state.py
+++ fight_state.py
@@ -9,6 +9,9 @@ class FightState(GameState):
             self.cursor_index = 0
             self.prev_key_w_state = False
             self.prev_key_s_state = False
+            self.prev_key_enter_state = False
+            self.end_of_fight = False
+            self.message = "You are in a fight!"
 
     def draw(self, enemy):
         draw_texture(load_texture('assets/fight.png'), 0, 0, WHITE)
@@ -17,17 +20,36 @@ class FightState(GameState):
         x = 640
         y = 65
 
+        if self.end_of_fight:
+            draw_text("You won! Press Enter to continue", 2 * 32, 10 * 32, 32, GREEN)
+            if is_key_down(KEY_ENTER) and not self.prev_key_enter_state:
+                self.end_of_fight = False
+                return Actions.EXPLORE
         options = ["ATTACK", "SPELL", "RUN AWAY"]
+        draw_text("Player HP: " + str(self.player.hp), 2 * 32, 12 * 32, 32, RED)
+        draw_text("Enemy HP: " + str(enemy.hp), 2 * 32, 13 * 32, 32, RED)
+        draw_text(self.message, 2 * 32, 14 * 32, 32, RED)
 
         key_w_state = is_key_down(KEY_W)
         key_s_state = is_key_down(KEY_S)
+        key_enter_state = is_key_down(KEY_ENTER)
 
         if ((key_w_state and not self.prev_key_w_state) or
                 (key_s_state and not self.prev_key_s_state)):
             self.cursor_index = self.move_cursor(self.cursor_index, len(options), "UP" if key_w_state else "DOWN")
 
+        if key_enter_state and not self.prev_key_enter_state:
+            if options[self.cursor_index] == "ATTACK":
+                dmg = self.player.do_attack()
+                self.message = "You attacked for " + str(dmg) + " damage"
+                draw_text(self.message, 2 * 32, 14 * 32, 32, RED)
+                enemy.apply_damage(dmg)
+                if not enemy.is_alive():
+                    self.end_of_fight = True
+
         self.prev_key_w_state = key_w_state
         self.prev_key_s_state = key_s_state
+        self.prev_key_enter_state = key_enter_state
 
         for index, option in enumerate(options):
             colour = RED

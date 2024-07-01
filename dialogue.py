62fda075aec466f85875852a92c85c5d272e1e03
zuvile <zivile.mitke@gmail.com>
2024-07-01 08:33:15 +0300

    add render variable to dialogue tree

--- dialogue.py
+++ dialogue.py
@@ -16,6 +16,7 @@ class DialogueTree:
         self.parent = None
         self.rel_mod = 0
         self.jmp = None
+        self.render = None
 
 
 class Dialogue:
@@ -35,31 +36,19 @@ class Dialogue:
             return root if root is not None else prev
         line = lines[idx].strip('\n').strip()
         if re.match('title: .*', line):
-            return self.create_tree(idx + 1, lines, prev, root)
+            return self.handle_title(idx, lines, prev, root)
         if line == 'end_choices':
-            return self.create_tree(idx + 1, lines, prev.parent.parent, root)
+            return self.handle_end_choices(idx, lines, prev, root)
         if re.match("(.*): (.*)", line):
-            speaker, dialogue = line.split(': ')
-            new_node = DialogueTree(dialogue, speaker)
-            if prev is not None:
-                prev.children.append(new_node)
-                new_node.parent = prev
-            if root is None:
-                root = new_node
-            return self.create_tree(idx + 1, lines, new_node, root)
-        if re.match("(\$rel_mod\s.*)", line):
-            _, modifier = line.split('=')
-            prev.rel_mod = int(modifier)
-            return self.create_tree(idx + 1, lines, prev, root)
-        if re.match("(jmp=.*)", line):
-            _, jmp = line.split('=')
-            prev.jmp = jmp
-            return self.create_tree(idx + 1, lines, prev, root)
+            return self.handle_speaker_dialogue(idx, lines, prev, root, line)
+        if re.match("(\\$rel_mod=.*)", line):
+            return self.handle_rel_mod(idx, lines, prev, root, line)
+        if re.match("(\\$jmp=.*)", line):
+            return self.handle_jmp(idx, lines, prev, root, line)
         if re.match("(->.*)", line.strip()):
-            new_node = DialogueTree(line, "Player")
-            new_node.parent = prev
-            prev.children.append(new_node)
-            return self.create_tree(idx + 1, lines, new_node, root)
+            return self.handle_choice(idx, lines, prev, root, line)
+        if re.match("(\\$render=.*)", line):
+            return self.handle_render(idx, lines, prev, root, line)
         return self.create_tree(idx + 1, lines, prev, root)
 
     def read_dialogue_chunks(self, lines, idx, dict, curr_title):
@@ -74,6 +63,43 @@ class Dialogue:
         else:
             dict[curr_title].append(line)
             self.read_dialogue_chunks(lines, idx + 1, dict, curr_title)
+
+    def handle_title(self, idx, lines, prev, root):
+        return self.create_tree(idx + 1, lines, prev, root)
+
+    def handle_end_choices(self, idx, lines, prev, root):
+        return self.create_tree(idx + 1, lines, prev.parent.parent, root)
+
+    def handle_speaker_dialogue(self, idx, lines, prev, root, line):
+        speaker, dialogue = line.split(': ')
+        new_node = DialogueTree(dialogue, speaker)
+        if prev is not None:
+            prev.children.append(new_node)
+            new_node.parent = prev
+        if root is None:
+            root = new_node
+        return self.create_tree(idx + 1, lines, new_node, root)
+
+    def handle_rel_mod(self, idx, lines, prev, root, line):
+        _, modifier = line.split('=')
+        prev.rel_mod = int(modifier)
+        print(prev.rel_mod)
+        return self.create_tree(idx + 1, lines, prev, root)
+
+    def handle_jmp(self, idx, lines, prev, root, line):
+        _, jmp = line.split('=')
+        prev.jmp = jmp
+        return self.create_tree(idx + 1, lines, prev, root)
+
+    def handle_choice(self, idx, lines, prev, root, line):
+        new_node = DialogueTree(line, "Player")
+        prev.children.append(new_node)
+        new_node.parent = prev
+        return self.create_tree(idx + 1, lines, new_node, root)
+
+    def handle_render(self, idx, lines, prev, root, line):
+        prev.render = line.split('=')[1]
+        return self.create_tree(idx + 1, lines, prev, root)
 
 
 def main():
--- dialogue_test.py
+++ dialogue_test.py
@@ -6,12 +6,13 @@ def dialogue():
     return Dialogue()
 
 
-def check_node(node, text, speaker, num_children, rel_mod, jmp):
+def check_node(node, text, speaker, num_children, rel_mod, jmp, render):
     assert node.text == text
     assert node.speaker == speaker
     assert len(node.children) == num_children
     assert node.rel_mod == rel_mod
     assert node.jmp == jmp
+    assert node.render == render
 
 
 def test_dialogue_tree(dialogue):
@@ -19,11 +20,11 @@ def test_dialogue_tree(dialogue):
     tree1 = trees['introduction_cassius']
     tree2 = trees['cassius_ceremony_room']
 
-    check_node(tree1, "Hello. I haven't seen you here before. My name is Cassius.", "Cassius", 1, 0, None)
-    check_node(tree1.children[0], "I have no time to chat to newcomers. One thing though... It would be unwise to go outside until you're ready.", "Cassius", 2, 0, None)
-    check_node(tree1.children[0].children[0], "-> Don't tell me what to do.", "Player", 1, -10, None)
-    check_node(tree1.children[0].children[0].children[0], "Ah, I see how it is. Well, don't let me stand in the way of your untimely death.", "Cassius", 0, 0, None)
-    check_node(tree1.children[0].children[1], "-> I see. Thank you.", "Player", 1, 10, None)
-    check_node(tree1.children[0].children[1].children[0], "Did you get the chance to see the ceremony room?", "Cassius", 0, 0, 'cassius_ceremony_room')
+    check_node(tree1, "Hello. I haven't seen you here before. My name is Cassius.", "Cassius", 1, 0, None, 'portrait_cassius.png')
+    check_node(tree1.children[0], "I have no time to chat to newcomers. One thing though... It would be unwise to go outside until you're ready.", "Cassius", 2, 0, None, None)
+    check_node(tree1.children[0].children[0], "-> Don't tell me what to do.", "Player", 1, -10, None, None)
+    check_node(tree1.children[0].children[0].children[0], "Ah, I see how it is. Well, don't let me stand in the way of your untimely death.", "Cassius", 0, 0, None, None)
+    check_node(tree1.children[0].children[1], "-> I see. Thank you.", "Player", 1, 10, None, None)
+    check_node(tree1.children[0].children[1].children[0], "Did you get the chance to see the ceremony room?", "Cassius", 0, 0, 'cassius_ceremony_room', None)
 
-    check_node(tree2, "It's a beautiful room, isn't it? I'm sure you'll be spending a lot of time there.", "Cassius", 0, 0, None)
+    check_node(tree2, "It's a beautiful room, isn't it? I'm sure you'll be spending a lot of time there.", "Cassius", 0, 0, None, None)
